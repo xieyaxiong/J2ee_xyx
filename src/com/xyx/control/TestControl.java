547c30e75190834ea947008d691235a9cd9de8cd
xieyaxiong <xieyaxiong@163.com>
2015-04-07 13:11:27 +0800

    jdbc

--- src/com/xyx/control/TestControl.java
+++ src/com/xyx/control/TestControl.java
@@ -1,5 +1,7 @@
 package com.xyx.control;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,10 +10,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class TestControl {
 	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
 	
 	@RequestMapping("xx.do")
 	public void xx(String xx){
-		System.out.println(xx);
+		System.out.println("save");
+		jdbcTemplate.update("insert into test(name) values('xx') ");
 	}
 
 }
